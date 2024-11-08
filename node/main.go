15773fa57585d6ad914f63fb5a7b3b7f9b8fca37
vmem <vmem@itu.dk>
2024-11-08 20:29:44 +0100

    critical section

--- node/main.go
+++ node/main.go
@@ -13,15 +13,37 @@ import (
 	"google.golang.org/grpc"
 )
 
+type Queue struct {
+	items []int32
+}
+
+func NewQueue() *Queue {
+	return &Queue{
+		items: make([]int32, 0),
+	}
+}
+
+func (q *Queue) Enqueue(id int32) {
+	q.items = append(q.items, id)
+}
+
+func (q *Queue) Dequeue() int32 {
+	if len(q.items) == 0 {
+		return -1 // empty queue
+	}
+	value := q.items[0]
+	q.items = q.items[1:]
+	return value
+}
+
 var (
 	serviceReg         = NewServiceRegistry()   // service registry for discovery
-	csMutex            = sync.Mutex{}           // mutex for critical section
-	csRequest          = []int32{}              // queue of requests for critical section
-	csUsed             = false                  // if critical section is in use
 	heartbeatTimeout   = 4 * time.Second        // max allowed interval between heartbeats
 	heartbeatInterval  = 1 * time.Second        // cooldown between heartbeats
 	rpcTimeout         = 200 * time.Millisecond // timeouts for rpc-calls using context.withTimeout()
 	randDeviationBound = 150                    // upper bound for random time deviation used in nowWithDeviation() (ms)
+	accessDuration     = 5 * time.Second        // time to hold the critical resource (s) NOTE: Must be lower than accessTimeout
+	accessTimeout      = 7 * time.Second        // if no response from holder, assume resource is free
 )
 
 type ServiceRegistry struct {
@@ -44,7 +66,10 @@ type Node struct {
 	leader        int32
 	term          int32
 	lastHeartbeat time.Time
-	isDead        bool // Gotta figure out how to actually stop the server
+	csQueue       *Queue
+	csIsUsedBy    int32
+	isWaiting     bool
+	lastCSAccess  time.Time
 }
 
 func NewNode(id int32, address string) *Node {
@@ -55,7 +80,10 @@ func NewNode(id int32, address string) *Node {
 		leader:        -1, // no leader when starting
 		term:          0,
 		lastHeartbeat: nowWithDeviation(),
-		isDead:        false,
+		csQueue:       NewQueue(),
+		csIsUsedBy:    -1,
+		isWaiting:     false,
+		lastCSAccess:  time.Now(),
 	}
 }
 
@@ -81,7 +109,7 @@ func (n *Node) start() {
 		fmt.Printf("Node %d failed to register\n", n.id)
 	}
 
-	go n.nodeLogic()
+	go n.nodeLogic(grpcServer)
 
 	if grpcServer.Serve(listener) != nil {
 		fmt.Printf("failed to serve: %v\n", err)
@@ -122,32 +150,49 @@ func (n *Node) addPeer(id int32, address string) {
 //                       Logic                       //
 ///////////////////////////////////////////////////////
 
-func (n *Node) nodeLogic() {
+func (n *Node) nodeLogic(srv *grpc.Server) {
 	for {
+		n.mu.Lock()
+
 		if !n.isLeader() {
+
+			if !n.isWaiting && n.leader != -1 {
+				n.requestResourceFromLeader()
+			}
+
 			if time.Since(n.lastHeartbeat) > heartbeatTimeout {
 				n.callElection()
 			}
+
+			n.mu.Unlock()
 			continue
 		}
 
+		if time.Since(n.lastCSAccess) > accessTimeout {
+			fmt.Printf("%d: Resource access timed out\n", n.id)
+			n.csIsUsedBy = -1
+		}
+
+		if n.csIsUsedBy == -1 {
+			n.informAccessToNext()
+		}
+
 		if time.Since(n.lastHeartbeat) < heartbeatInterval {
+			n.mu.Unlock()
 			continue
 		}
 
 		// 1/10 chance of dying
 		if rand.IntN(10) == 1 {
-			n.isDead = true
 			fmt.Printf("%d died\n", n.id)
+			srv.Stop()
+			n.mu.Unlock()
 			return
 		}
 
-		// Send heartbeat
-		for _, peer := range n.peers {
-			peer.TransmitHeartbeat(context.Background(), &pb.Request{Sender: n.id, Term: n.term})
-		}
-
+		n.heartbeat()
 		n.lastHeartbeat = nowWithDeviation()
+		n.mu.Unlock()
 	}
 }
 
@@ -155,10 +200,64 @@ func (n *Node) isLeader() bool {
 	return n.leader == n.id
 }
 
-func (n *Node) callElection() {
-	n.mu.Lock()
-	defer n.mu.Unlock()
+func (n *Node) requestResourceFromLeader() {
+	ctx, cancel := context.WithTimeout(context.Background(), rpcTimeout)
+	req := &pb.Request{Sender: n.id}
+	defer cancel()
 
+	reply, err := n.peers[n.leader].RequestResource(ctx, req)
+	if err == nil && reply.Granted {
+		n.isWaiting = true
+	}
+}
+
+func (n *Node) informAccessToNext() {
+	nextId := n.csQueue.Dequeue()
+
+	if nextId == -1 || n.peers[nextId] == nil {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), rpcTimeout)
+	req := &pb.Empty{}
+	defer cancel()
+
+	_, err := n.peers[nextId].InformAccess(ctx, req)
+	if err != nil {
+		return
+	}
+
+	n.csIsUsedBy = nextId
+	n.lastCSAccess = time.Now()
+}
+
+func (n *Node) heartbeat() {
+	majority := len(n.peers)/2 + 1
+	nSuccess := 1 // heartbeat to self always succesfull
+
+	req := &pb.HeartBeat{Sender: n.id, Term: n.term, Queue: n.csQueue.items, CsIsUsedBy: n.csIsUsedBy}
+	ctx, cancel := context.WithTimeout(context.Background(), rpcTimeout)
+	defer cancel()
+
+	for _, peer := range n.peers {
+		reply, err := peer.TransmitHeartbeat(ctx, req)
+		if err != nil {
+			continue
+		}
+		if !reply.Granted {
+			n.leader = -1
+			// Maybe increase own term
+			return
+		}
+		nSuccess++
+	}
+
+	if nSuccess < majority {
+		n.leader = -1
+	}
+}
+
+func (n *Node) callElection() {
 	fmt.Printf("%d is calling an election in term %d\n", n.id, n.term+1)
 
 	majority := len(n.peers)/2 + 1
@@ -187,6 +286,29 @@ func (n *Node) callElection() {
 	n.lastHeartbeat = nowWithDeviation()
 }
 
+func (n *Node) holdResource() {
+	fmt.Printf("%d: Im using the critical resource!!\n", n.id)
+	time.Sleep(accessDuration)
+	fmt.Printf("%d: I have released the critical resource!!\n", n.id)
+
+	for {
+		n.mu.Lock()
+		if n.leader == -1 || n.peers[n.leader] == nil {
+			n.mu.Unlock()
+			break
+		}
+		_, err := n.peers[n.leader].InformRelease(context.Background(), &pb.Request{Sender: n.id})
+		if err == nil {
+			n.mu.Unlock()
+			break
+		}
+		fmt.Printf("%d: Failed to inform release, retrying...\n", n.id)
+		n.mu.Unlock()
+
+		time.Sleep(500 * time.Millisecond) // wait before retrying
+	}
+}
+
 ///////////////////////////////////////////////////////
 //                rpc-implementations                //
 ///////////////////////////////////////////////////////
@@ -199,10 +321,6 @@ func (n *Node) InformArrival(_ context.Context, in *pb.NodeInfo) (*pb.Empty, err
 func (n *Node) RequestVote(_ context.Context, in *pb.Request) (*pb.Reply, error) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
-
-	if n.isDead {
-		return &pb.Reply{Granted: false}, nil
-	}
 
 	if n.term > in.Term {
 		fmt.Printf("%d: Request from %d is bad. Got term: %d, expected term: %d\n", n.id, in.Sender, in.Term, n.term)
@@ -222,41 +340,56 @@ func (n *Node) RequestVote(_ context.Context, in *pb.Request) (*pb.Reply, error)
 	return &pb.Reply{Granted: true}, nil
 }
 
-func (n *Node) TransmitHeartbeat(_ context.Context, in *pb.Request) (*pb.Empty, error) {
+func (n *Node) TransmitHeartbeat(_ context.Context, in *pb.HeartBeat) (*pb.Reply, error) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	if n.isDead {
-		return &pb.Empty{}, nil
-	}
 	if n.term > in.Term {
 		fmt.Printf("%d: The heartbeat was bad. Got term: %d, expected term: %d\n", n.id, in.Term, n.term)
-		return &pb.Empty{}, nil
+		return &pb.Reply{Granted: false}, nil
 	}
 
 	n.leader = in.Sender
 	n.term = in.Term
+	n.csQueue.items = in.Queue
+	n.csIsUsedBy = in.CsIsUsedBy
 	n.lastHeartbeat = nowWithDeviation()
 
-	fmt.Printf("%d: has recieved heartbeat from %d in term %d\n", n.id, in.Sender, n.term)
+	//fmt.Printf("%d: has recieved heartbeat from %d in term %d\n", n.id, in.Sender, n.term)
 
-	return &pb.Empty{}, nil
+	return &pb.Reply{Granted: true}, nil
 }
 
-func (n *Node) RequestAccess(_ context.Context, in *pb.Request) (*pb.Reply, error) {
+func (n *Node) RequestResource(_ context.Context, in *pb.Request) (*pb.Reply, error) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
 	if !n.isLeader() {
 		return &pb.Reply{Granted: false}, nil
 	}
-	if csUsed {
-		csRequest = append(csRequest, in.Sender) // add node to queue
-		return &pb.Reply{Granted: false}, nil
+
+	n.csQueue.Enqueue(in.Sender)
+	return &pb.Reply{Granted: true}, nil
+}
+
+func (n *Node) InformAccess(_ context.Context, in *pb.Empty) (*pb.Empty, error) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.isWaiting = false
+	go n.holdResource()
+	return &pb.Empty{}, nil
+}
+
+func (n *Node) InformRelease(_ context.Context, in *pb.Request) (*pb.Empty, error) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if n.csIsUsedBy == in.Sender {
+		n.csIsUsedBy = -1
 	}
 
-	csMutex.Lock()
-	csUsed = true
-	fmt.Println("CS is now in use")
-	csMutex.Unlock()
-	return &pb.Reply{Granted: true}, nil
+	return &pb.Empty{}, nil
 }
 
 ///////////////////////////////////////////////////////
